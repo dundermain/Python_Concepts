b9b6eefc24eea2470abaf870cc8afd0a76416cad
SynapticLad <sachin68834@gmail.com>
2023-05-20 23:21:31 +0530

    private and protected attribute

--- OOPS_Python/OOPS_Encapsulation.py
+++ OOPS_Python/OOPS_Encapsulation.py
@@ -14,8 +14,17 @@ class SoftwareEngineer:
         #double underscore (e.g. __x) is called "private attribute"
         self._salary =  None
         self._num_bugs_solved = 0
+        
+    #now lets assume someone wants to access the private tags
+    def get_salary(self):
+        return self._salary
+    
+    def set_salary(self, value):
+        self._salary = value
 
 
 
 se = SoftwareEngineer("Max", 25)
 print(se.age, se.name)
+se.set_salary(6000)
+print(se.get_salary())
