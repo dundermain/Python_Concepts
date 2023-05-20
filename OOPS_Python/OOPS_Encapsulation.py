74441ba9b4efdd78fd120d97170b2c2e01124d0b
SynapticLad <sachin68834@gmail.com>
2023-05-20 23:12:33 +0530

    Added encapsulation

--- OOPS_Python/OOPS_Encapsulation.py
+++ OOPS_Python/OOPS_Encapsulation.py
@@ -1,4 +1,19 @@
 #Here we will discuss about encapsulation and abstarction
 
 #Encapsulation is a mechanism of hiding of data implementation. Instance variables/methods can be hidden or made private. 
-#This can only be accessed by getter setter method
+#This can only be accessed by accessor 
+
+class SoftwareEngineer:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age =  age
+
+        #Lets salary and number of bugs solved attribute to private
+        self._salary =  None
+        self._num_bugs_solved = 0
+
+
+
+se = SoftwareEngineer("Max", 25)
+print(se.age, se.name)
