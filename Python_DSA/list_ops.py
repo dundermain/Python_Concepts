84d4188ac3f790e37a1e2ca67432acb4679b06e4
SynapticLad <sachin68834@gmail.com>
2023-05-24 22:28:11 +0530

    added list operations

--- Python_DSA/list_ops.py
+++ Python_DSA/list_ops.py
@@ -6,13 +6,18 @@ mylist = ['a', 'b', 'c', 'd', 'e']
 
 mylist.pop(2) #This will delete the element at index 2 and will return the updated list
 #if we donot give any index, .pop() will delete the last element
+#this method has space complexity of O(1) as no extra memory is being created
+#it has time complexity of O(n) of we are speicifying index in pop and if we are not specifying any index then it is O(1)
 
 #another method to delete element is del method
 del mylist[2]
 #with delete method we can delete multiple elements using indexing
 del mylist[2:4]
+#This has time complextiy of O(n)
 
-
+#ANother method is remove method. list.remove(element) is used when we donot know the index of element we want to delete
+mylist.remove('e')
+#This has time complextiy of O(n)
 
 
 
