092abd72d90a2febfa02916e48a3ee9a33b712ec
SynapticLad <sachin68834@gmail.com>
2023-05-24 22:20:53 +0530

    added list operations

---
+++ Python_DSA/list_ops.py
@@ -0,0 +1,12 @@
+#This is from Udemy DSA Course
+
+mylist = ['a', 'b', 'c', 'd', 'e']
+
+#list.pop(index) method is used to delete the element present in that index
+
+mylist.pop(2) #This will delete the element at index 2 and will return the updated list
+
+
+
+
+
