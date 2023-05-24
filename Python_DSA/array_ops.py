d678a31e2a4a7582bb2b487c2b094479d6873538
SynapticLad <sachin68834@gmail.com>
2023-05-24 22:38:20 +0530

    added array  operations

---
+++ Python_DSA/array_ops.py
@@ -0,0 +1,15 @@
+#This is from Udemy DSA Course
+
+import array
+my_array1 = array.array('i', [1,2,3,4])
+
+#to insert an element in array
+
+
+#to search an element in an array
+def linear_search(arr, target):
+    for i in range(len(array)):
+        if arr[i] == target:
+            return i
+        else:
+            return "Element not present in array"
