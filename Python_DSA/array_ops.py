afd46dc199dbe44c154ea86a16c163c84e34c498
SynapticLad <sachin68834@gmail.com>
2023-05-24 22:43:03 +0530

    added array  operations

--- Python_DSA/array_ops.py
+++ Python_DSA/array_ops.py
@@ -3,8 +3,8 @@
 import array
 my_array1 = array.array('i', [1,2,3,4])
 
-#to insert an element in array
-
+#to insert an element in array we can use array.insert(index, element)
+my_array1.insert(0, 6)
 
 #to search an element in an array
 def linear_search(arr, target):
