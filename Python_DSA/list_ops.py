505dfefa9ad24069bac51e3acfcd12759f90700d
SynapticLad <sachin68834@gmail.com>
2023-05-24 22:23:24 +0530

    added list operations

--- Python_DSA/list_ops.py
+++ Python_DSA/list_ops.py
@@ -5,6 +5,14 @@ mylist = ['a', 'b', 'c', 'd', 'e']
 #list.pop(index) method is used to delete the element present in that index
 
 mylist.pop(2) #This will delete the element at index 2 and will return the updated list
+#if we donot give any index, .pop() will delete the last element
+
+#another method to delete element is del method
+del mylist[2]
+#with delete method we can delete multiple elements using indexing
+del mylist[2:4]
+
+
 
 
 
